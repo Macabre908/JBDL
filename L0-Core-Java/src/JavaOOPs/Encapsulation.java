0e9e2143dca8e104484fe34973f4696b21e32e6c
Abhishek Dubey <abhishekdubey789901@gmail.com>
2025-11-25 12:47:22 +0530

    Added additional parameters to test Encapsulation.java using Getters and Setters.

--- L0-Core-Java/src/JavaOOPs/Encapsulation.java
+++ L0-Core-Java/src/JavaOOPs/Encapsulation.java
@@ -1,8 +1,9 @@
 package JavaOOPs;
 
 class Student{
-    String name;
+    private String name;
     private float CGPA;
+    private int age;
 
     public float getCGPA(){
         return this.CGPA;
@@ -11,12 +12,31 @@ class Student{
     public void setCGPA(float CGPA){
         this.CGPA = CGPA;
     }
+
+    public int getAge(){
+        return this.age;
+    }
+
+    public void setAge(int age){
+        this.age = age;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
 }
 public class Encapsulation {
     public static void main(String []args){
         Student s1 = new Student();
         s1.setCGPA(9.7f);
+        s1.setAge(18);
+        s1.setName("John Doe");
 
-        System.out.println(s1.getCGPA());
+        System.out.println(s1.getName() + " is " + s1.getAge() + " years old and scored a CGPA of " + s1.getCGPA());
     }
 }
